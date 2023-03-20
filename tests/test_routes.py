f24c4d58f3bfe787aa90c0d6ea1508b89d93b36b
Eng. Emad Kamel <emad.mathew@gmail.com>
2023-03-20 23:30:46 +0200

    rollback main

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -126,7 +126,16 @@ class TestAccountService(TestCase):
         )
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
-    def test_read_an_account(self):
+    # ADD YOUR TEST CASES HERE ...
+    def test_get_account_list(self):
+        """It should get a list of accounts"""
+        self._create_accounts(5)
+        resp = self.client.get(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(len(data), 5)
+
+    def test_get_account(self):
         """It should Read a single Account"""
         account = self._create_accounts(1)[0]
         resp = self.client.get(
@@ -162,13 +171,14 @@ class TestAccountService(TestCase):
         resp = self.client.delete(f"{BASE_URL}/{account.id}")
         self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
 
-    def test_get_account_list(self):
-        """It should Get a list of Accounts"""
-        self._create_accounts(5)
-        resp = self.client.get(BASE_URL)
-        self.assertEqual(resp.status_code, status.HTTP_200_OK)
-        data = resp.get_json()
-        self.assertEqual(len(data), 5)
+    def test_method_not_allowed(self):
+        """It should not allow an illegal method call"""
+        resp = self.client.delete(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    #######################################################################
+    # S E C U R I T Y   T E S T   C A S E S
+    #######################################################################
 
     def test_security_headers(self):
         """It should return security headers"""
